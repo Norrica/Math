e6288972ce6ea5c1a35f1a2c2ceef0996f2fb496
gladisar <gladisar@student.21-school.ru>
2022-06-26 10:59:07 +0300

    Initial commit

---
+++ src/s21_math.c
@@ -0,0 +1,17 @@
+#include <s21_math.h>
+
+int abs(int x) { return 0; }
+long double acos(double x) { return 0; }
+long double asin(double x) { return 0; }
+long double atan(double x) { return 0; }
+long double ceil(double x) { return 0; }
+long double cos(double x) { return 0; }
+long double exp(double x) { return 0; }
+long double fabs(double x) { return 0; }
+long double floor(double x) { return 0; }
+long double fmod(double x, double y) { return 0; }
+long double log(double x) { return 0; }
+long double pow(double base, double exp) { return 0; }
+long double sin(double x) { return 0; }
+long double sqrt(double x) { return 0; }
+long double tan(double x) { return 0; }
---
+++ src/s21_math.h
@@ -0,0 +1,20 @@
+#ifndef SRC_S21_MATH_H_
+#define SRC_S21_MATH_H_
+
+int abs(int x);                            // computes absolute value of an integer value
+long double acos(double x);                // computes arc cosine
+long double asin(double x);                // computes arc sine
+long double atan(double x);                // computes arc tangent
+long double ceil(double x);                // returns the nearest integer not less than the given value
+long double cos(double x);                 // computes cosine
+long double exp(double x);                 // returns e raised to the given power
+long double fabs(double x);                // computes absolute value of a floating-point value
+long double floor(double x);               // returns the nearest integer not greater than the given value
+long double fmod(double x, double y);      // remainder of the floating-point division operation
+long double log(double x);                 // computes natural logarithm
+long double pow(double base, double exp);  // raises a number to the given power
+long double sin(double x);                 // computes sine
+long double sqrt(double x);                // computes square root
+long double tan(double x);                 // computes tangent
+
+#endif  // SRC_S21_MATH_H_
