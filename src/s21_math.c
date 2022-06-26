259b9a4a04f4c510c9d4fe7b1d145d7734224d3d
gladisar <gladisar@student.21-school.ru>
2022-06-26 20:38:46 +0300

    s21_

--- src/s21_math.c
+++ src/s21_math.c
@@ -1,17 +1,17 @@
 #include <s21_math.h>
 
-int abs(int x) { return 0; }
-long double acos(double x) { return 0; }
-long double asin(double x) { return 0; }
-long double atan(double x) { return 0; }
-long double ceil(double x) { return 0; }
-long double cos(double x) { return 0; }
-long double exp(double x) { return 0; }
-long double fabs(double x) { return 0; }
-long double floor(double x) { return 0; }
-long double fmod(double x, double y) { return 0; }
-long double log(double x) { return 0; }
-long double pow(double base, double exp) { return 0; }
-long double sin(double x) { return 0; }
-long double sqrt(double x) { return 0; }
-long double tan(double x) { return 0; }
+int s21_abs(int x) { return 0; }
+long double s21_acos(double x) { return 0; }
+long double s21_asin(double x) { return 0; }
+long double s21_atan(double x) { return 0; }
+long double s21_ceil(double x) { return 0; }
+long double s21_cos(double x) { return 0; }
+long double s21_exp(double x) { return 0; }
+long double s21_fabs(double x) { return 0; }
+long double s21_floor(double x) { return 0; }
+long double s21_fmod(double x, double y) { return 0; }
+long double s21_log(double x) { return 0; }
+long double s21_pow(double base, double exp) { return 0; }
+long double s21_sin(double x) { return 0; }
+long double s21_sqrt(double x) { return 0; }
+long double s21_tan(double x) { return 0; }
