bc1e5d670adebf2bf0a4c76656d98d58a38da7d0
Jarrus Abram <jarrusab@pr-d1.kzn.21-school.ru>
2022-06-27 16:59:41 +0300

    added cos (not finished), changed fmod

--- src/s21_math.c
+++ src/s21_math.c
@@ -11,7 +11,25 @@ long double s21_acos(double x) { return 0; }
 long double s21_asin(double x) { return 0; }
 long double s21_atan(double x) { return 0; }
 long double s21_ceil(double x) { return (long double)((int)x + 1); }
-long double s21_cos(double x) { return 0; }
+long double s21_cos(double x) {
+    /*
+      алгоритм вычисления косинуса через ряд тейлора
+     */
+    int sign = 1;
+    x = s21_fmod(x, S21_PI * 2);
+    /* if (s21_fabs(x - S21_1_PI) < 1e-17) { */
+    /*     x -= S21_1_PI; */
+    /*     sign = -1; */
+    /* } */
+    double x_2 = x * x;
+    long double last_term = -1 * (x_2 / 2);
+    long double result = 1 + last_term;
+    for (int i = 2; i  <= NUM_OF_TERMS; i++) {
+        last_term = last_term * ((x_2 * (-1)) / ((i * 2 - 1) * (i * 2)));
+        result += last_term;
+    }
+    return sign * result;
+}
 long double s21_exp(double x) { return 0; }
 long double s21_fabs(double x) {
     if (x < 0) {
@@ -22,20 +40,7 @@ long double s21_fabs(double x) {
 }
 long double s21_floor(double x) { return (long double)((int)x); }
 long double s21_fmod(double x, double y) {
-    int isneg = 0;
-    if (x < 0 && y < 0) {
-        isneg = 1;
-    }
-    x = s21_fabs(x);
-    y = s21_fabs(y);
-    // printf("x :>> %f\n", x);
-    // printf("y :>> %f\n", y);
-    while (x > y) {
-        x -= y;
-    }
-
-    if (isneg) x = -x;
-    return x;
+    return ((x) - (int)((x) / (y)) * (y));
 }
 long double s21_log(double x) { return 0; }
 long double s21_pow(double base, double exp) { return 0; }
